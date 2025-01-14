241da59ba16c44c0dd054d68fd88ffb5f6836e9d
Cynic <69977714+Cynic1254@users.noreply.github.com>
2025-01-14 10:15:31 +0100

    maybe this

--- src/com/github/cynic1254/HelloWorld.groovy
+++ src/com/github/cynic1254/HelloWorld.groovy
@@ -1,6 +1,12 @@
 package com.github.cynic1254
 
+import jenkins.Steps
+
 class HelloWorld {
+    Steps getSteps() {
+        return new Steps()
+    }
+
     void Print() {
         Steps.echo("Test")
     }
